faf4fccd4b034bbff1830c4b37dc0c82f6493511
YeeunJ <21800669@handong.edu>
2020-08-08 01:07:02 +0900

    db info

--- routes/basic.js
+++ routes/basic.js
@@ -13,11 +13,15 @@ const db = new sqlite3.Database('./resources/db/information.db', sqlite3.OPEN_RE
 
 router.get('/', function(req, res) {
   const query1 = `select * from setting where id = 1;`;
-  const query2 = `select * from cam_image where peopleCNT is not null order by cameraID, regDate;`;
-  const query3 = `select cameraID, substr(regDate, 1, 10) as date, sum(peopleCNT) as people from cam_image where peopleCNT is not null and strftime('%j', regDate) > strftime('%j', datetime('now', 'localtime'), '-7 days') GROUP BY strftime('%j', regDate), cameraID order by cameraID, date;`;
-  const query4 = `select cameraID, substr(regDate, 1, 16) as date, sum(peopleCNT) as people from cam_image where peopleCNT is not null and strftime('%j', regDate) > strftime('%j', datetime('now', 'localtime'), '-1 days') GROUP BY strftime('%H', regDate), cameraID order by cameraID, date;;`;
-  const query5 = `select cameraID, substr(regDate, 1, 16) as date, count(*) as count, avg(peopleCNT) as people from cam_image where peopleCNT is not null and strftime('%H', regDate) >= strftime('%H', datetime('now', 'localtime'), '-1 hours') and strftime('%j', regDate) > strftime('%j', datetime('now', 'localtime'), '-1 days') GROUP BY strftime('%M', regDate), cameraID order by cameraID, date;;`;
-  const query6 = `select * from cam_image where peopleCNT is not null and regDate = (SELECT max(regDate) from cam_image where peopleCNT is not null);`;
+  const query2 = `select * from cam_image where peopleCNT is not null order by cameraID, originalDate;`;
+  const query3 = `select cameraID, substr(originalDate, 1, 10) as date, sum(peopleCNT) as people from cam_image where peopleCNT is not null and strftime('%j', regDate) > strftime('%j', datetime('now', 'localtime'), '-7 days') GROUP BY strftime('%j', originalDate), cameraID order by cameraID, date;`;
+  const query4 = `select cameraID, substr(originalDate, 1, 16) as date, sum(peopleCNT) as people from cam_image where peopleCNT is not null and strftime('%j', regDate) > strftime('%j', datetime('now', 'localtime'), '-1 days') GROUP BY strftime('%H', originalDate), cameraID order by cameraID, date;`;
+  const query5 = `select cameraID, substr(originalDate, 1, 16) as date, count(*) as count, avg(peopleCNT) as people from cam_image where peopleCNT is not null and strftime('%H', originalDate) >= strftime('%H', datetime('now', 'localtime'), '-1 hours') and strftime('%j', originalDate) > strftime('%j', datetime('now', 'localtime'), '-1 days') GROUP BY strftime('%M', originalDate), cameraID order by cameraID, date;`;
+  const query6 = `select * from cam_image where peopleCNT is not null and originalDate = (SELECT max(originalDate) from cam_image where peopleCNT is not null);`;
+  console.log(query2);
+  console.log(query3);
+  console.log(query4);
+  console.log(query5);
   db.parallelize(() => {
     db.each(query1, (err, rows1) => {
       db.all(query2, (err, rows2) => {
@@ -54,10 +58,10 @@ router.post('/search', function(req, res) {
       const {starttime, endtime} = req.body;
 
       const query1 = `select * from setting where id = 1;`;
-      const query2 = `select * from cam_image where regDate between '${starttime}' and '${endtime}';`;
-      const query3 = `select cameraID, substr(regDate, 1, 10) as date, sum(peopleCNT) as people from cam_image where strftime('%j', regDate) > strftime('%j', '${endtime}', '-7 days') and regDate between '${starttime}' and '${endtime}' GROUP BY strftime('%j', regDate), cameraID;`;
-      const query4 = `select cameraID, substr(regDate, 1, 13) as date, sum(peopleCNT) as people from cam_image where strftime('%j', regDate) > strftime('%j', '${endtime}', '-1 days') and regDate between '${starttime}' and '${endtime}' GROUP BY strftime('%H', regDate), cameraID;`;
-      const query5 = `select cameraID, substr(regDate, 1, 16) as date, avg(peopleCNT) as people from cam_image where strftime('%H', regDate) > strftime('%H', '${endtime}', '-1 hours') and strftime('%j', regDate) > strftime('%j', '${endtime}', '-1 days') and regDate between '${starttime}' and '${endtime}' GROUP BY strftime('%M', regDate), cameraID;`;
+      const query2 = `select * from cam_image where originalDate between '${starttime}' and '${endtime}';`;
+      const query3 = `select cameraID, substr(originalDate, 1, 10) as date, sum(peopleCNT) as people from cam_image where strftime('%j', originalDate) > strftime('%j', '${endtime}', '-7 days') and originalDate between '${starttime}' and '${endtime}' GROUP BY strftime('%j', originalDate), cameraID;`;
+      const query4 = `select cameraID, substr(originalDate, 1, 13) as date, sum(peopleCNT) as people from cam_image where strftime('%j', originalDate) > strftime('%j', '${endtime}', '-1 days') and originalDate between '${starttime}' and '${endtime}' GROUP BY strftime('%H', originalDate), cameraID;`;
+      const query5 = `select cameraID, substr(originalDate, 1, 16) as date, avg(peopleCNT) as people from cam_image where strftime('%H', originalDate) > strftime('%H', '${endtime}', '-1 hours') and strftime('%j', originalDate) > strftime('%j', '${endtime}', '-1 days') and originalDate between '${starttime}' and '${endtime}' GROUP BY strftime('%M', originalDate), cameraID;`;
       console.log(query2);
       console.log(query3);
       console.log(query4);
