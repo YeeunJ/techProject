36a3a3ac675e743295839ff2b0422cab32cbae6a
YeeunJ <21800669@handong.edu>
2020-08-24 14:57:51 +0900

    chart update

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,47 +1,51 @@
-//var ctx = document.getElementById('myChart1').getContext('2d');
-window.onload = function() {
-  document.getElementById('myChart4').style.display = 'none';
-  var ctx = document.getElementById('myChart1').getContext('2d');
-  window.myLine = Chart.Line(ctx, {
-    data: chart1,
-    options: {
-      responsive: true,
-      hoverMode: 'index',
-      stacked: false,
-      title: {
-        display: false,
-      },
-      scales: {
-        yAxes: [{
-          type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-          display: true,
-          position: 'left',
-          id: 'y-axis-1',
-          gridLines:{
-            color: 'rgba(255, 255, 255, 0.4)',
-            lineWidth:1
-          }
-        }],
-        ticks: {
-          stepSize: 10,
-          beginAtZero: false,
-					fontSize : 16,
-          padding: '80px',
-        }
-      },
-      legend: {
-               display: true,
-               fontSize : 20,
-               position: 'top',
-               align: 'start',
-               labels: {
-                usePointStyle: 'true'
+console.log('test');
+console.log(data3);
+$(document).ready(function() {
+  $('#myChart4').hide();
+  var ctx = $('#myChart1');
+
+  var myChart = new Chart(ctx, {
+      type: 'line',
+      data: chart1,
+      options: {
+        responsive: true,
+        hoverMode: 'index',
+        stacked: false,
+        title: {
+          display: false,
+        },
+        scales: {
+          yAxes: [{
+            type: 'linear',
+            display: true,
+            position: 'left',
+            id: 'y-axis-1',
+            gridLines:{
+              color: 'rgba(255, 255, 255, 0.4)',
+              lineWidth:1
             }
-           }
-    }
+          }],
+          ticks: {
+            stepSize: 10,
+            beginAtZero: false,
+  					fontSize : 16,
+            padding: '80px',
+          }
+        },
+        legend: {
+                 display: true,
+                 fontSize : 20,
+                 position: 'top',
+                 align: 'start',
+                 labels: {
+                  usePointStyle: 'true'
+              }
+             }
+      }
   });
-  var ctx = document.getElementById('myChart2').getContext('2d');
-  window.myLine = Chart.Line(ctx, {
+  var ctx = $('#myChart2');
+  var myChart = new Chart(ctx, {
+    type: 'line',
     data: chart2,
     options: {
       responsive: true,
@@ -76,10 +80,11 @@ window.onload = function() {
            }
     }
   });
-  document.getElementById('myChart2').style.display = 'none';
-  var ctx = document.getElementById('myChart3').getContext('2d');
+  $('#myChart2').hide();
+  var ctx = $('#myChart3');
 
-  window.myLine = Chart.Line(ctx, {
+  var myChart = new Chart(ctx, {
+    type: 'line',
     data: chart3,
     options: {
       responsive: true,
@@ -115,32 +120,33 @@ window.onload = function() {
            }
     }
   });
-  document.getElementById('myChart3').style.display = 'none';
-}
-  document.getElementById('chartBtn1').addEventListener('click', function() {
-    document.getElementById('myChart3').style.display = 'none';
-    document.getElementById('myChart2').style.display = 'none';
-    document.getElementById('myChart4').style.display = 'none';
-    document.getElementById('myChart1').style.display = 'block';
-    document.getElementById('chartBtn1').className = 'clickedCriteria';
-    document.getElementById('chartBtn2').className = 'criteria';
-    document.getElementById('chartBtn3').className = 'criteria';
+  $('#myChart3').hide();
+});
+
+  $('#chartBtn1').click(function(){
+    $('#myChart1').show();
+    $('#myChart2').hide();
+    $('#myChart3').hide();
+    $('#myChart4').hide();
+    $('#chartBtn1').attr('class', 'clickedCriteria');
+    $('#chartBtn2').attr('class', 'criteria');
+    $('#chartBtn3').attr('class', 'criteria');
   });
-  document.getElementById('chartBtn2').addEventListener('click', function() {
-    document.getElementById('myChart1').style.display = 'none';
-    document.getElementById('myChart3').style.display = 'none';
-    document.getElementById('myChart4').style.display = 'none';
-    document.getElementById('myChart2').style.display = 'block';
-    document.getElementById('chartBtn2').className = 'clickedCriteria';
-    document.getElementById('chartBtn1').className = 'criteria';
-    document.getElementById('chartBtn3').className = 'criteria';
+  $('#chartBtn2').click(function(){
+    $('#myChart1').hide();
+    $('#myChart2').show();
+    $('#myChart3').hide();
+    $('#myChart4').hide();
+    $('#chartBtn2').attr('class', 'clickedCriteria');
+    $('#chartBtn1').attr('class', 'criteria');
+    $('#chartBtn3').attr('class', 'criteria');
   });
-  document.getElementById('chartBtn3').addEventListener('click', function() {
-      document.getElementById('myChart1').style.display = 'none';
-      document.getElementById('myChart2').style.display = 'none';
-      document.getElementById('myChart4').style.display = 'none';
-      document.getElementById('myChart3').style.display = 'block';
-      document.getElementById('chartBtn3').className = 'clickedCriteria';
-      document.getElementById('chartBtn1').className = 'criteria';
-      document.getElementById('chartBtn2').className = 'criteria';
-    });
+  $('#chartBtn3').click(function(){
+    $('#myChart1').hide();
+    $('#myChart2').hide();
+    $('#myChart3').show();
+    $('#myChart4').hide();
+    $('#chartBtn3').attr('class', 'clickedCriteria');
+    $('#chartBtn1').attr('class', 'criteria');
+    $('#chartBtn2').attr('class', 'criteria');
+  });
