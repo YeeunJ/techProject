c605726031c632c7c27cb06b9d6ccdbd868a488b
YeeunJ <21800669@handong.edu>
2020-08-27 14:50:54 +0900

    rm opencv

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var sqlite3 = require('sqlite3').verbose();
 var fs = require('fs');
-var addon = require('bindings')('../resources/cpp/build/Release/people-detector');
+//var addon = require('bindings')('../resources/cpp/build/Release/people-detector');
 
 var setting_id = 1;
 
@@ -73,14 +73,14 @@ router.post('/basic/image-info', function(req, res) {
   require("fs").writeFile("resources/images/original/" + filename, base64Data, 'base64', function(err) {
     if (err === null) {
       db.all(query3, (err, row) => {
-
+/*
               var obj = new addon.Yolo_cpu();
               if(row){
                 var people = obj.start("resources/images/original/" + filename, "resources/images/result/" + filename, 416, {"data": row});
               }else{
                 var people = obj.start("resources/images/original/" + filename, "resources/images/result/" + filename, 416);
               }
-
+*/
               const query1 = `insert into cam_image (name, originalDate, cameraID, peopleCNT, settingID)
                 values ("${originalDate}_${cameraID}.jpeg", "${originalDate}", ${cameraID}, ${people}, ${setting_id});`;
               db.each(query1, (err, row) => {
