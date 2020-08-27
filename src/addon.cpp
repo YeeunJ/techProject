693dc4ecf8cdffffc6919b7abd8ad448f189e0b6
YeeunJ <21800669@handong.edu>
2020-08-27 20:19:54 +0900

    test

--- src/addon.cpp
+++
@@ -1,8 +0,0 @@
-#include <napi.h>
-#include "yolo_cpu.hpp"
-
-Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
-    return Yolo_cpu::Init(env, exports);
-}
-
-NODE_API_MODULE(addon, InitAll)
