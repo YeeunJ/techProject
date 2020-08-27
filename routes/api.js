0b754ca64bedc3d436802fffc135c10eb6026405
YeeunJ <21800669@handong.edu>
2020-08-27 16:06:22 +0900

    test

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var sqlite3 = require('sqlite3').verbose();
 var fs = require('fs');
-//var addon = require('bindings')('../resources/cpp/build/Release/people-detector');
+var addon = require('bindings')('../resources/cpp/build/Release/people-detector');
 
 var setting_id = 1;
 
@@ -73,20 +73,23 @@ router.post('/basic/image-info', function(req, res) {
   require("fs").writeFile("resources/images/original/" + filename, base64Data, 'base64', function(err) {
     if (err === null) {
       db.all(query3, (err, row) => {
-/*
-              var obj = new addon.Yolo_cpu();
-              if(row){
-                var people = obj.start("resources/images/original/" + filename, "resources/images/result/" + filename, 416, {"data": row});
-              }else{
-                var people = obj.start("resources/images/original/" + filename, "resources/images/result/" + filename, 416);
-              }
-*/
-              const query1 = `insert into cam_image (name, originalDate, cameraID, peopleCNT, settingID)
-                values ("${originalDate}_${cameraID}.jpeg", "${originalDate}", ${cameraID}, ${people}, ${setting_id});`;
-              db.each(query1, (err, row) => {
-                if (err) return res.json(err);
-              });
-      });
+        try {
+          var obj = new addon.Yolo_cpu();
+          if(row){
+            var people = obj.start("resources/images/original/" + filename, "resources/images/result/" + filename, 416, {"data": row});
+          }else{
+            var people = obj.start("resources/images/original/" + filename, "resources/images/result/" + filename, 416);
+          }
+        } catch (e) {
+          alert(`다음과 같은 에러가 발생했습니다: ${e.name}: ${e.message}`);
+          var people = 0;
+        }
+        const query1 = `insert into cam_image (name, originalDate, cameraID, peopleCNT, settingID)
+            values ("${originalDate}_${cameraID}.jpeg", "${originalDate}", ${cameraID}, ${people}, ${setting_id});`;
+        db.each(query1, (err, row) => {
+          if (err) return res.json(err);
+        });
+    });
     } else {
       console.log('fail');
     }
